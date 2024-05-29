b9e474c4299a0f636860a1941adb3d4864cf940a
nicholasshelnutt <shelnuttnq@appstate.edu>
2024-05-29 14:13:02 -0400

    Added required java files.

---
+++ src/Car.java
@@ -0,0 +1,7 @@
+public class Car extends Vehicle
+{
+    public void vehicleType()
+    {
+        System.out.println("You chose a vehicle of type Car.");
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,7 @@
+public class Truck extends Vehicle
+{
+    public void vehicleType()
+    {
+        System.out.println("You chose a vehicle of type Truck.");
+    }
+}
